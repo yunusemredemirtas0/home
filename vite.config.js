2b26b67f4f3ad953883125b4614acd1994b61be0
Yunus Emre DEMİRTAŞ <yunusemredemirtas.dev@gmail.com>
2026-01-18 21:48:24 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          firebase: ['firebase/app', 'firebase/firestore', 'firebase/auth']
+        }
+      }
+    }
+  }
 })
