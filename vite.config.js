3ba2ac3bf12392bcf60112a7695b7b394b3f001d
yunusemredemirtas0 <yunusemredemirtas.dev@gmail.com>
2026-02-19 14:19:36 +0300

    Web uzerinden otomatik guncelleme: 2026-02-19 14:19

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:11434',
+        target: 'http://ollama:11434',
         changeOrigin: true,
       },
     },
